572332b7ffa6ea849ef2962303b52ad8b16c7d7f
4motionEnjoyer <leevi.sulonen@protonmail.com>
2024-01-15 00:01:45 +0200

    Update sähkönhinta.py
    Added +-1 day and cleaned up the code

--- sähkönhinta.py
+++ sähkönhinta.py
@@ -1,15 +1,32 @@
+#!/bin/python
 #
-#Sähkönhinta widgetti, aina tälle päivälle
+##################################################
+#Sähkönhinta widgetti, tänään +-1 pvä
 #By 4motionEnjoyer / Leevi S.
-#
+###################################################
 
+##############
+#Importit
+##############
 import requests
 import datetime
 import json
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+from tkinter import *
 
+matplotlib.use('TkAgg')
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg,
+    NavigationToolbar2Tk
+)
+
+##############
+#Funktiot
+##############
 
 def luo_graafi(eri_päivä = 0):
     if eri_päivä != 0:
@@ -19,17 +36,17 @@ def luo_graafi(eri_päivä = 0):
     
     pvm = kloaika.split()[0]
 
-        
+    #print(pvm)
+  
     api_url = "https://www.sahkohinta-api.fi/api/v1/kallis?tunnit=24&tulos=sarja&aikaraja=" + pvm
 
     vastaus = requests.get(api_url)
     binaari = vastaus.content
     ulos_json = json.loads(binaari)
-
     ulos_list = list(ulos_json)
-
     ulos_tunnit = []
     ulos_hinnat = []
+
     for tunti in ulos_list:
         als = str(tunti.get("aikaleima_suomi")) #als aikaleimasuomi
         als_trimmed = als.split("T")
@@ -37,19 +54,88 @@ def luo_graafi(eri_päivä = 0):
         ulos_tunnit.append(als_trimmed[1].split(":")[0])
         ulos_hinnat.append(hinta)
 
+    #print(ulos_hinnat)
 
-    plt.bar(ulos_tunnit, ulos_hinnat)
-    plt.xlabel("Kellonaika")
-    plt.ylabel("Hinta [snt/kWh]")
-    plt.title("Sähkön hinta " + pvm)
-    return plt, pvm
-    #plt.show()
+    return ulos_tunnit, ulos_hinnat, pvm
+
+
+def aiempi_päivämäärä():
+    global mikäpvä
+    mikäpvä -= 1
+    if (mikäpvä == -1):
+        vasen_nappi["state"] = DISABLED
+    if (mikäpvä < 1):
+        oikea_nappi["state"] = NORMAL
+    eri_päivä = mikäpvä
+    piirturi(eri_päivä)
+
+
+def seuraava_päivämäärä():
+    global mikäpvä
+    mikäpvä += 1
+    if (mikäpvä == 1):
+        oikea_nappi["state"] = DISABLED
+    if (mikäpvä < 1):
+        vasen_nappi["state"] = NORMAL
+    eri_päivä = mikäpvä
+    piirturi(eri_päivä)
+
+
+def piirturi(eri_päivä = 0):
+    ulos_tunnit, ulos_hinnat, pvm = luo_graafi(eri_päivä)  
+    teksti.delete("1.0", END)
+    teksti.insert("end", pvm + " sähkön hinta")
+
+    global figure
+    figure.clear()
+
+    axes = figure.add_subplot()
+    axes.bar(ulos_tunnit, ulos_hinnat)
+    axes.set_xlabel("Tunnit")
+    axes.set_ylabel("Hinta [snt/kWh]")
+    figure.canvas.draw_idle()
+    figure_canvas.get_tk_widget().pack(side= "bottom", fill= "both", expand=1) 
+
+
+def sulku():
+    root.destroy()
+    exit()
+
 
 def main():
-    plot, pvm = luo_graafi()
-    plot.show()
+    ulos_tunnit, ulos_hinnat, pvm = luo_graafi()
+    piirturi()
+    root.mainloop()
 
-    exit()
+####################
+#Globaalit muuttujat
+####################
+
+pvm = str(datetime.datetime.now()).split()[0]
+root = Tk()
+
+#mpl canvas asetukset
+figure = Figure(figsize=(6, 4), dpi=100)
+figure_canvas = FigureCanvasTkAgg(figure, root)
+
+# Ikkunan asetukset
+root.geometry('800x450') 
+root.title("Sähkön hinta")
+root.protocol("WM_DELETE_WINDOW", sulku)
+
+# UI olioiden luonti
+vasen_nappi = Button(root, text = "<", bd= "5", command = aiempi_päivämäärä)
+oikea_nappi = Button(root, text = ">", bd= "5", command = seuraava_päivämäärä)
+teksti = Text(root, height = 1, width = 40)
+vasen_nappi.pack(side = "left")
+oikea_nappi.pack(side = "right")
+teksti.pack(side = "top")
+teksti.insert("end", pvm + " sähkön hinta")
+
+mikäpvä = 0
     
-main()
-
+#Voidaan ajaa vain suoraan invokoituna
+if __name__ == "__main__": 
+    main()
+else:
+    exit()
