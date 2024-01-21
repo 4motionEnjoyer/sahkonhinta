5c4192f724ec548356071dc31f08a702fbfe3604
leevisulonen <leevisulonen@leevinkone2.twin.net>
2024-01-21 23:46:08 +0200

    Added wd thread to block out next day button when outside of time window Sw now downloads all three day's data uses less api calls. error message box when things go south general improvements to logic and algorithms

--- README.md
+++ README.md
@@ -2,6 +2,14 @@ Näyttää sähkön hinnan matplotlib kuvaajana.
 Pystyy katsomaan myös eilisen ja huomisen hinnat.  
 Hakee datan https://www.sahkohinta-api.fi  
 
+Toteutetuneet ominaisuudet:
+Varoituslaatikko jos jotain menee pieleen
+Debug ja offline testausmahdollisuus
+Sovelluksen käynnistyessä -1, 0 ja +1 päivien datan imuttaminen joten päivien välillä liikkuminen on jouhevampaa
+
+Suunnitellut ominaisuudet:
+tietojen tallentaminen paikallisesti niin vielä vähemmä api kutsuja
+jonkinlainen trendiviiva parin viimeviikon ajalta / uusi ali-ikkuna kokonaan. oman nappulan takana.
 
 
 ![image](https://github.com/4motionEnjoyer/sahkonhinta/assets/99203040/b0337cf8-d097-4222-a69b-3a4cfde31c1a)
--- sähkönhinta.py
+++ sähkönhinta.py
@@ -8,6 +8,8 @@
 ##############
 #Importit
 ##############
+import random
+import sys
 import requests
 import datetime
 import json
@@ -15,6 +17,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from tkinter import *
+from tkinter import messagebox
+import threading
+import time
 
 matplotlib.use('TkAgg')
 
@@ -28,33 +33,117 @@ from matplotlib.backends.backend_tkagg import (
 #Funktiot
 ##############
 
-def luo_graafi(eri_päivä = 0):
-    if eri_päivä != 0:
-        kloaika = str(datetime.datetime.now() + datetime.timedelta(days=eri_päivä))
-    else: 
-        kloaika = str(datetime.datetime.now())
+def thread_kellonkyttäys():
+    while(True):
+        if datetime.datetime.now().time() > datetime.time(0,0, 0) and datetime.datetime.now().time() < datetime.time(14, 15, 00) and pvm_diff == 0:
+            oikea_nappi["state"] = DISABLED
+        else:
+            oikea_nappi["state"] = NORMAL 
+        sleep(60)
+
+
+def varoitus_ikkuna():
+#    päällimmäinen = Toplevel(root)
+#    päällimmäinen.geometry("400x150")
+#    päällimmäinen.title("Varoitus")
+#    Label(päällimmäinen, text = "Jostain syystä dataa ei saatu. Nordpool julkaisee seuraavan päivän datat ~14:00")
+    v_ikkuna = messagebox.showerror("Varoitus", "Sovellus ei saanut dataa netistä")
+    if v_ikkuna == "No":
+        exit()
+    else:
+        pass
+
+def keksi_data():
+    global pävät_tietue
+    global pvm
+    #päivät_tietue["ulos_tunnit"] = list(range(0,24))
+    print (päivät_tietue["ulos_tunnit"])
+ #   päivät_tietue["ulos_hinnat_eilen"] = []
+  #  päivät_tietue["ulos_hinnat_tänään"] = []
+   # päivät_tietue["ulos_hinnat_huomenna"] = []
     
-    pvm = kloaika.split()[0]
+#    ulos_tunnit = range(0, 24)
+#    ulos_hinnat_eilen = []
+#    ulos_hinnat_tänään = []
+#    ulos_hinnat_huomenna = []
 
-    #print(pvm)
-  
-    api_url = "https://www.sahkohinta-api.fi/api/v1/kallis?tunnit=24&tulos=sarja&aikaraja=" + pvm
 
-    vastaus = requests.get(api_url)
-    binaari = vastaus.content
+    while(len(päivät_tietue["ulos_hinnat_tänään"]) < 24):
+        päivät_tietue["ulos_hinnat_eilen"].append(random.randrange(1, 30))
+        päivät_tietue["ulos_hinnat_tänään"].append(random.randrange(1, 30))
+        päivät_tietue["ulos_hinnat_huomenna"].append(random.randrange(1, 30))
+
+    pvm = str(datetime.datetime.now() + datetime.timedelta(days=0)).split()[0]
+    #return hinta_tietue
+
+
+def hae_data():
+
+
+    global päivät_tietue
+    global pvm
+    pvm = str(datetime.datetime.now() + datetime.timedelta(days=0)).split()[0]
+    if db_lippu: print (päivät_tietue["ulos_tunnit"])
+
+    api_url_eilen = "https://www.sahkohinta-api.fi/api/v1/kallis?tunnit=24&tulos=sarja&aikaraja=" + str(datetime.datetime.now() + datetime.timedelta(days=-1)).split()[0]
+    api_url_tänään = "https://www.sahkohinta-api.fi/api/v1/kallis?tunnit=24&tulos=sarja&aikaraja=" +  str(datetime.datetime.now() + datetime.timedelta(days=0)).split()[0]
+    api_url_huomenna = "https://www.sahkohinta-api.fi/api/v1/kallis?tunnit=24&tulos=sarja&aikaraja=" + str(datetime.datetime.now() + datetime.timedelta(days=1)).split()[0]
+
+    vastaus_eilen = requests.get(api_url_eilen)
+    vastaus_tänään = requests.get(api_url_tänään)
+    vastaus_huomenna = requests.get(api_url_huomenna)
+
+    binaari_eilen = vastaus_eilen.content
+    binaari_tänään = vastaus_tänään.content
+    binaari_huomenna = vastaus_huomenna.content
+
     try:
-        ulos_json = json.loads(binaari)
-        ulos_list = list(ulos_json)
+        ulos_json_eilen = json.loads(binaari_eilen)
+        ulos_json_tänään = json.loads(binaari_tänään)
+        if datetime.datetime.now().time() > datetime.time(14,15,0):
+            ulos_json_huomenna = json.loads(binaari_huomenna)
+        else:
+            ulos_json_huomenna = "before14"
+        ulos_list_eilen = list(ulos_json_eilen)
+        ulos_list_tänään = list(ulos_json_tänään)
+        ulos_list_huomenna = list(ulos_json_eilen)
 
-        ulos_tunnit = []
-        ulos_hinnat = []
+        ulos_tunnit_eilen = []
+        ulos_tunnit_tänään = []
+        ulos_tunnit_huomenna = []
 
-        for tunti in ulos_list:
+
+        ulos_hinnat_eilen = []
+        ulos_hinnat_tänään = []
+        ulos_hinnat_huomenna = []
+
+        for tunti in ulos_list_eilen:
             als = str(tunti.get("aikaleima_suomi")) #als aikaleimasuomi
             als_trimmed = als.split("T")
             hinta = float(tunti.get("hinta"))
-            ulos_tunnit.append(als_trimmed[1].split(":")[0])
-            ulos_hinnat.append(hinta)
+            ulos_tunnit_eilen.append(als_trimmed[1].split(":")[0])
+            ulos_hinnat_eilen.append(hinta)
+        päivät_tietue["ulos_hinnat_eilen"] = ulos_hinnat_eilen
+        if db_lippu: print(päivät_tietue["ulos_hinnat_eilen"])
+        
+        for tunti in ulos_list_tänään:      #boilerplate, vois tehdä viisaamminki
+            als = str(tunti.get("aikaleima_suomi")) #als aikaleimasuomi
+            als_trimmed = als.split("T")
+            hinta = float(tunti.get("hinta"))
+            ulos_tunnit_tänään.append(als_trimmed[1].split(":")[0])
+            ulos_hinnat_tänään.append(hinta)
+        päivät_tietue["ulos_hinnat_tänään"] = ulos_hinnat_tänään
+        if db_lippu: print(str(päivät_tietue["ulos_hinnat_tänään"]))
+
+        for tunti in ulos_list_huomenna:
+            als = str(tunti.get("aikaleima_suomi")) #als aikaleimasuomi
+            als_trimmed = als.split("T")
+            hinta = float(tunti.get("hinta"))
+            ulos_tunnit_huomenna.append(als_trimmed[1].split(":")[0])
+            ulos_hinnat_huomenna.append(hinta)
+        päivät_tietue["ulos_hinnat_huomenna"] = ulos_hinnat_huomenna
+        if db_lippu: print(str(päivät_tietue["ulos_hinnat_huomenna"]))
+        
 
     except Exception as e:
         print("Joko käytit liikaa API kutsuja (60 tunnissa sallittu) tai huomisen dataa ei ole julkaistu, yleensä klo 14:00 eteenpäin. Alla tulkin sepustus")
@@ -62,47 +151,80 @@ def luo_graafi(eri_päivä = 0):
         print(str(e))
         print("\nYritä myödemmin uudelleen")
         aiempi_päivämäärä()
-    
-    
- 
+        varoitus_ikkuna()
 
     #print(ulos_hinnat)
 
-    return ulos_tunnit, ulos_hinnat, pvm
+    #return ulos_tunnit, ulos_hinnat_eilen, ulos_hinnat_tänään, ulos_hinnat_huomenna, pvm
 
 
 def aiempi_päivämäärä():
-    global mikäpvä
-    mikäpvä -= 1
-    if (mikäpvä == -1):
+    global pvm_diff
+    pvm_diff -= 1
+    if (pvm_diff == -1):
         vasen_nappi["state"] = DISABLED
-    if (mikäpvä < 1):
+    if (pvm_diff < 1):
         oikea_nappi["state"] = NORMAL
-    eri_päivä = mikäpvä
-    piirturi(eri_päivä)
+
+    piirturi()
 
 
 def seuraava_päivämäärä():
-    global mikäpvä
-    mikäpvä += 1
-    if (mikäpvä == 1):
-        oikea_nappi["state"] = DISABLED
-    if (mikäpvä < 1):
-        vasen_nappi["state"] = NORMAL
-    eri_päivä = mikäpvä
-    piirturi(eri_päivä)
+    global pvm_diff
+    global keksi_lippu
+    global db_lippu
+    if keksi_lippu and db_lippu:    #testing purposes
+        varoitus_ikkuna()
+    else:
+        pvm_diff += 1
+        if (pvm_diff == 1):
+            oikea_nappi["state"] = DISABLED
+        if (pvm_diff < 1):
+            vasen_nappi["state"] = NORMAL
+        
+        piirturi()
 
 
-def piirturi(eri_päivä = 0):
-    ulos_tunnit, ulos_hinnat, pvm = luo_graafi(eri_päivä)  
+#def piirturi(eri_päivä = 0):
+def piirturi(): 
+    global keksi_lippu
+    global pvm_diff
+    global pvm
+    global päivät_tietue
+
+    #global ulos_hinnat_eilen, ulos_hinnat_tänään, ulos_hinnat_huomenna
+#    if not keksi_lippu:
+#        ulos_tunnit, ulos_hinnat, pvm = hae_data(eri_päivä)  
+#         ulos_tunnit, ulos_hinnat_eilen, ulos_hinnat_tänään, ulos_hinnat_huomenna = hae_data() 
+#       pass 
+#else:
+#        ulos_tietue = keksi_data()
+
     teksti.delete("1.0", END)
-    teksti.insert("end", pvm + " sähkön hinta")
+    teksti.insert("end", str(datetime.datetime.now().date() + datetime.timedelta(days = pvm_diff)) + " sähkön hinta")
 
     global figure
     figure.clear()
 
     axes = figure.add_subplot()
-    axes.bar(ulos_tunnit, ulos_hinnat)
+    match pvm_diff:
+        case -1:
+            ulos_hinnat = päivät_tietue["ulos_hinnat_eilen"]
+            if db_lippu: print("case -1")
+        case 0:
+            ulos_hinnat = päivät_tietue["ulos_hinnat_tänään"]
+            if db_lippu: print("case 0")
+        case 1:
+            ulos_hinnat = päivät_tietue["ulos_hinnat_huomenna"]
+            if db_lippu: print("case + 1")
+
+    if db_lippu:
+        print("tyyppi " + str(type(päivät_tietue["ulos_hinnat_eilen"])) + " pituus" + str(len(päivät_tietue["ulos_hinnat_eilen"])))
+        print("tyyppi " + str(type(ulos_hinnat)) + " pituus " + str(len(ulos_hinnat)))
+        print("ulos_hinnat_eilen on: " + str(päivät_tietue["ulos_hinnat_eilen"]))
+        print("ulos_hinnat_tänää on: " + str(päivät_tietue["ulos_hinnat_tänään"]))
+        print("ulos_hinnat_huome on: " + str(päivät_tietue["ulos_hinnat_huomenna"]))
+    axes.bar(päivät_tietue["ulos_tunnit"], ulos_hinnat)
     axes.set_xlabel("Tunnit")
     axes.set_ylabel("Hinta [snt/kWh]")
     figure.canvas.draw_idle()
@@ -113,17 +235,52 @@ def sulku():
     root.destroy()
     exit()
 
+def argumenttiparsija():
+    argumentit = []
+    for argumentti in sys.argv:
+        argumentit.append(argumentti)
+    return argumentit, len(argumentit)
+
 
 def main():
-    ulos_tunnit, ulos_hinnat, pvm = luo_graafi()
+    argumentit, alkm = argumenttiparsija()
+    global päivät_tietue
+    if "db" in argumentit:
+        global db_lippu 
+        db_lippu = True
+    if "offline" in argumentit:
+#        ulos_tunnit, ulos_hinnat, pvm = keksi_data()
+        global keksi_lippu
+        keksi_lippu = True
+        keksi_data()
+    else:
+        hae_data()
+
     piirturi()
+    vk_säie = threading.Thread(target = thread_kellonkyttäys)
+    vk_säie.start()
     root.mainloop()
 
 ####################
 #Globaalit muuttujat
 ####################
 
+päivät_tietue = {}
+
+päivät_tietue["ulos_tunnit"] = [] 
+päivät_tietue["ulos_tunnit"] = list(range(0,24))
+päivät_tietue["ulos_hinnat_eilen"] = []
+päivät_tietue["ulos_hinnat_tänään"] = []
+päivät_tietue["ulos_hinnat_huomenna"] = []
+
+#ulos_hinnat_eilen = []
+#ulos_hinnat_tänään = []
+#ulos_hinnat_huomenna = []
+
+db_lippu = False
+keksi_lippu = False
 pvm = str(datetime.datetime.now()).split()[0]
+pvm_diff = 0
 root = Tk()
 
 #mpl canvas asetukset
